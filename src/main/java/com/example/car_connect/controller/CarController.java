ff3ab8be1b185631611a8b7128b96569cff9052f
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-10-11 23:37:48 +0600

    Created enpoint to get car's images

--- src/main/java/com/example/car_connect/controller/CarController.java
+++ src/main/java/com/example/car_connect/controller/CarController.java
@@ -1,21 +1,30 @@
 package com.example.car_connect.controller;
 
 import com.example.car_connect.model.dto.car.CarRegisterRequest;
+import com.example.car_connect.model.dto.image.CarImageResponse;
+import com.example.car_connect.service.CarImageService;
 import com.example.car_connect.service.CarService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/cars")
 public class CarController {
     private CarService carService;
+    private CarImageService carImageService;
 
     @PostMapping("/register")
     public void register(@RequestBody CarRegisterRequest request) {
         carService.register(request);
+    }
+
+    @GetMapping("/images/{carId}")
+    public List<CarImageResponse> getImages(@PathVariable UUID carId) {
+        return carService.getImages(carId);
     }
 }
